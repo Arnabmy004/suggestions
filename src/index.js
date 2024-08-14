678acafe2dc80e7b024d7ba8075949f43a376f77
Arnab <arnab1090.be22@chitkarauniversity.edu.in>
2024-08-14 10:43:13 +0530

    hi

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 
  const  input=document.getElementById('input-field');
+const resultcontainer=document.getElementById('result-container');
 
-
+let timeout=null;
  function display(){
-    setTimeout(fetchData,1000);
+    clearTimeout(timeout);
+   timeout= setTimeout(()=>{
+      fetchData(input.value);
+   },500);
  }
 
- async function fetchData(term) {
+async function fetchData(value) {
      
-    let res= await fetch(`https://dummyjson.com/products/search?q=${input.value}`);
+    let res= await fetch(`https://dummyjson.com/products/search?q=${value}`);
     let result=await res.json();
-    console.log(result);
+   
+    let products=result.products;
+    //console.log(products);
+  resultcontainer.innerHTML = "";
+    products.forEach((v)=> {
+     const div=document.createElement('div');
+      div.innerText=`${v.title}`;
+      resultcontainer.appendChild(div);
+      console.log(v.title);
+     
+    });
+}
 
- }
- fetchData();
 
 
